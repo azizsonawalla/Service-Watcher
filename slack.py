0353a648057fe1003ec6f82029cb48fcce33cb0d
Aziz Sonawalla <35513492+azizsonawalla@users.noreply.github.com>
2018-12-20 16:02:58 -0800

    working on slack notifications

--- slack.py
+++ slack.py
@@ -6,3 +6,26 @@ class Slack:
 	def __init__(self, webhook):
 		self.webhook = webhook
 
+	def send_message(self, msg):
+		print("Sending Slack notification")
+		sys.stdout.flush()
+
+		headers = {
+		    "Accept": "application/json"
+		}
+
+		query = {
+		    "username": self.slack["username"],
+		    "channel": self.slack["channel"],
+		    "icon_emoji": self.slack["icon"],
+		    "text": msg,
+		}
+
+		try:
+		    requests.request("POST", self.slack["url"], headers=headers, data=json.dumps(query), verify=True)
+		except Exception as e:
+		    print("** Failed to send Slack notification {}".format(str(e)))
+		    sys.stdout.flush()
+
+		print("** Successfully sent Slack notification")
+		
